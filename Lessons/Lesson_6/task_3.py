4eec3b611a1c47ddee94309b74b5366f7f20a64e
EgorMalinovsky <malinovskyegor93@gmail.com>
2024-11-20 20:50:04 +0300

    Добавление задачи 3

--- Lessons/Lesson_6/task_3.py
+++ Lessons/Lesson_6/task_3.py
@@ -4,3 +4,22 @@ Task_3
 Реализовать функцию, которая определяет, является ли это число простым
 (делится только на единицу и на само себя).
 """
+
+# Ввод пользователем числа
+number_input = int(input(f"Введите число для проверки является ли оно простым: "))
+
+
+# Функция на проверку того, является ли число простым
+def checking_prime_number(number):
+    if number <= 1:
+        return False
+    for i in range(2, number // 2+1):
+        if number % i == 0:
+            return False
+    return True
+
+
+if checking_prime_number(number_input):
+    print(f"Число {number_input} не является простым")
+else:
+    print(f"Число {number_input} является простым")
